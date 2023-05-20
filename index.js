efecc89815146c127375fe397389cd58a4fee91a
5haddam <131292268+5haddam@users.noreply.github.com>
2023-05-20 21:26:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function removeElement(array, value) {
+    index = array.indexOf(value);
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
